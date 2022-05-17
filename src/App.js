b52a23b1f4aad6fb806a067ffc877bfb20731ee8
Laurynas Valatka <laurynas.valatka@balt.net>
2022-05-17 17:22:57 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import React from 'react';
 function App() {
   const value = 'World';
   return <div>Hello double {value}</div>;
+  <input type="text" name="myText" value="Norway" selectBoxOptions="Canada;Denmark;Finland;Germany;Mexico;Norway;Sweden;United Kingdom;United States">
+   <script type="text/javascript">
+createEditableSelect(document.forms[0].myText);
+</script> 
+  </input>
 }
+
+
 
 export default App;
