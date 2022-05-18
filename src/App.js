45125332196e0c9e7eb6da6c3a5a60881bde3c8e
Laurynas Valatka <laurynas.valatka@balt.net>
2022-05-18 13:29:49 +0300

    test4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div><h1></h1>Hello double {value}</h1></div>;
+  return <div><h1>Hello double {value}</h1></div>;
 
 }
 
